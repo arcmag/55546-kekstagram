56d598dd22c92f3bee315d28201cbcec870f0d1e
Николай <arcmag-work@mail.ru>
2018-12-07 01:57:23 +0700

    Выполняет шаги из задания - #23 Личный проект: надо подкачаться

---
+++ js/backend.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+  var URL = 'https://js.dump.academy/kekstagram';
+
+  function load(onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      onLoad(xhr.response);
+    });
+    xhr.addEventListener('error', onError);
+    xhr.open('GET', URL + '/data');
+
+    xhr.send();
+  }
+
+  function save(data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', onLoad);
+    xhr.addEventListener('error', onError);
+
+    xhr.open('POST', URL);
+
+    xhr.send(data);
+  }
+
+  window.backend = {
+    load: load,
+    save: save
+  };
+}());
